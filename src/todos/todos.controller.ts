1b4d91cd0bab525769c08de7201ca9dcbea9b4cc
Funncy <gywns41@naver.com>
2021-09-14 10:10:34 +0900

    Implement : create todo & findAll todos

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TodosService } from './todos.service';
+import { Todo } from './interfaces/todo.interface';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
 export class TodosController {
-  constructor(private catsService: TodosService) {}
+  constructor(private todosService: TodosService) {}
+
+  @Post()
+  async create(@Body() createTodoDto: CreateTodoDto) {
+    this.todosService.create(createTodoDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Todo[]> {
+    return this.todosService.findAll();
+  }
 }
